6d6c4995e166246ff2c603d7d2485c0e03fa2840
Arunan <arunan.selvarajah@mail.utoronto.ca>
2025-10-22 20:53:03 -0400

    Assignment is completed

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -38,27 +38,24 @@ mv rawdata data/raw
 ls data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-mkdir -p data/processed/{server_logs,user_logs,event_logs}
+mkdir data/processed
+mkdir data/processed/server_logs
+mkdir data/processed/user_logs
+mkdir data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp data/raw/*server*.log data/processed/server_logs 2>/dev/null
+cp data/raw/*server*.log data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
-cp data/raw/*user*.log data/processed/user_logs 2>/dev/null
-cp data/raw/*event*.log data/processed/event_logs 2>/dev/null
+cp data/raw/*user*.log data/processed/user_logs
+cp data/raw/*event*.log data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-<<<<<<< HEAD
-rm -f data/raw/*ipaddr*
-rm -f data/processed/user_logs/*ipaddr*
-=======
-rf -rf ./data
->>>>>>> ea20676d33161a6f4d0fcd3c4f7aa5360f0f4309
+rm -rf data/processed/user_logs/*ipaddr*
+rm -rf data/raw/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 find data/processed -type f > data/inventory.txt
-echo "The project setup is complete."
-
 
 ###########################################
 
